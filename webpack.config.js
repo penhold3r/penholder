4307bf6f27e0359f3f35a491598a8079cbc43eeb
penholder <penhold3r@gmail.com>
2019-06-27 03:38:50 -0300

    env variables config

--- webpack.config.js
+++ webpack.config.js
@@ -14,11 +14,31 @@ module.exports = env => {
 	const envDef = `${path.join(__dirname)}/.env`
 	const envMode = `${envDef}.${env}`
 	const envFile = fs.existsSync(envMode) ? envMode : envDef
-	const envParsed = dotenv.config({ path: envFile }).parsed
-	const envKeys = Object.keys(envParsed).reduce((agr, curr) => {
-		agr[`process.env.${curr}`] = JSON.stringify(envParsed[curr])
-		return agr
-	}, {})
+	console.log('----------------------\n', envFile)
+
+	let envKeys
+	if (fs.existsSync(envFile)) {
+		console.log('\x1b[36m%s\x1b[0m', `>>>>>>> ENV:`)
+		const envParsed = dotenv.config({ path: envFile }).parsed
+		console.log('----------------------\n', envParsed, '\n----------------------')
+		envKeys = Object.keys(envParsed).reduce((agr, curr) => {
+			console.log('----------------------\n', agr)
+			agr[`process.env.${curr}`] = JSON.stringify(envParsed[curr])
+			return agr
+		}, {})
+		console.log('----------------------\n', envKeys)
+		console.log('----------------------\n')
+	}
+
+	const wpPlugins = [
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'css/ph.style.css'
+		})
+	]
+	fs.existsSync(envFile) && wpPlugins.push(new webpack.DefinePlugin(envKeys))
 
 	return {
 		entry: './src/index.js',
@@ -94,14 +114,6 @@ module.exports = env => {
 		devServer: {
 			historyApiFallback: true
 		},
-		plugins: [
-			new webpack.DefinePlugin(envKeys),
-			new HtmlWebpackPlugin({
-				template: './src/index.html'
-			}),
-			new MiniCssExtractPlugin({
-				filename: 'css/ph.style.css'
-			})
-		]
+		plugins: wpPlugins
 	}
 }
